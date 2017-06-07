c99893464b7b720c121144584d5a641332606724
Sverhoef <sjors_verhoef@hotmail.com>
2017-06-07 12:32:24 +0200

    Emoticon functie toegevoegd, was een fout in if statements in de library. Canvas image toegevoegd alleen emoticon nog uitlijnen voor prototype.

--- index.php
+++ index.php
@@ -3,92 +3,86 @@
 <html>
     <head>
         <title>library index</title>
-        <script type="text/javascript" src="library.js"></script>
+        <script type="text/javascript" language="JavaScript" src="library.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+
     </head>
     <body>
-        
-        <script>
-
-       
-        </script>
-        
-        
-        
-    <canvas id="visualisation" width="500%"; height="auto";>
-    
-    </canvas>
-        
-        
-        <script>
-        var c = document.getElementById("visualisation");
-        var ctx = c.getContext("2d");
-
-
-            
-            ctx.beginPath();
-            ctx.arc(100,75,Module.bmiToSize(<?php echo $_GET["BMI"];?>),0,2*Math.PI);
-            ctx.fillStyle = Module.hexToRgb(<?php echo $_GET["haircolor"];?>);
-            ctx.fill();
-            /*
-            //HAPPY
-            //oog links
-            ctx.beginPath();
-            ctx.arc(90,75,5,0,2*Math.PI);
-            ctx.fillStyle = "Black";
-            ctx.fill();
-            //oog rechts
-            ctx.beginPath();
-            ctx.arc(110,75,5,0,2*Math.PI);
-            ctx.fillStyle = "Black";
-            ctx.fill();
-            //mond
-            ctx.beginPath();
-            ctx.arc(100, 90, 20, 0, Math.PI, false);
-            ctx.fillStyle = "Black";
-            ctx.fill();
-            //HAPPY
-            */
-            /*
-            //SHOCKED
-            //oog links
-            ctx.beginPath();
-            ctx.arc(90,75,5,0,2*Math.PI);
-            ctx.fillStyle = "Black";
-            ctx.fill();
-            //oog rechts
-            ctx.beginPath();
-            ctx.arc(110,75,5,0,2*Math.PI);
-            ctx.fillStyle = "Black";
-            ctx.fill();
-            //mond
-            ctx.beginPath();
-            ctx.arc(100, 105, 20, 0, Math.PI, true);
-            ctx.fillStyle = "Black";
-            ctx.fill();
-            //SHOCKED
-            */
-            
-                 Module.emotion("<?php echo $_GET["facialExpression"];?>");
-       
-            
-            ctx.beginPath();
-            ctx.arc(250,75,Module.bmiToSize(<?php echo $_GET["BMI"];?>),0,2*Math.PI);
-            ctx.stroke();
-            ctx.fillStyle = Module.hexToRgb(<?php echo $_GET["skintone"];?>);
-            ctx.fill();
-            
-        </script>
-        
-        <form action="" method="GET">
-            hair color: <br><input type="text" name="haircolor" value="995000"><br>
-            clothes color: <br><input type="text" name="skintone" value="678392"><br>
-            BMI: <br><input type="text" name="BMI" value="20"><br>
+     <div id="lowercenter">
+		 <canvas id="canvas" width="800%"; height="300px"; style="background-color:#d9d9d9;"></canvas><br>
+	
+		<form action="" method="GET">
+            hair color: <br><input type="text" name="haircolor" value="<?php if(isset($_GET["haircolor"])){echo $_GET["haircolor"];}else{echo "995000";}?>"><br>
+            clothes color: <br><input type="text" name="skintone" value="<?php if(isset($_GET["skintone"])){echo $_GET["skintone"];}else{echo "678392";}?>"><br>
+            BMI: <br><input type="text" name="BMI" value="<?php if(isset($_GET["BMI"])){echo $_GET["BMI"];}else{echo "20";}?>"><br>
             <select name="facialExpression">
-                <option value="happy">happy</option>
-                <option value="shocked">shocked</option>
+                <option value="1">happy</option>
+                <option value="2">shocked</option>
             </select>
             <input type="submit" value="Submit">
         </form>
+        
+		
+		
+		
+		<div id="canvasdiv">
+    	<canvas id="haircolor" width="155%"; height="auto"; style="background-color:#d9d9d9;"> </canvas>
+        <script>
+        var c = document.getElementById("haircolor");
+        var ctx = c.getContext("2d");
+
+		ctx.beginPath();
+		ctx.arc(75,75,Module.bmiToSize(<?php echo $_GET["BMI"];?>),0,2*Math.PI);
+		ctx.fillStyle = Module.hexToRgb(<?php echo $_GET["haircolor"];?>);
+		ctx.fill();		
+		ctx.closePath
+		
+		</script>
+		
+		
+		<canvas id="skintone" width="155%"; height="auto"; style="background-color:#d9d9d9;"> </canvas>
+		<script>
+		var c = document.getElementById("skintone");
+        var ctx = c.getContext("2d");
+			
+		ctx.beginPath();
+		ctx.arc(75,75,Module.bmiToSize(<?php echo $_GET["BMI"];?>),0,2*Math.PI);
+		ctx.fillStyle = Module.hexToRgb(<?php echo $_GET["skintone"];?>);
+		ctx.fill();
+		ctx.closePath; 
+		</script>
+		
+		<canvas id="emotion" width="155%"; height="auto"; style="background-color:#d9d9d9;"> </canvas>
+		<script>
+		var c = document.getElementById("emotion");
+        var ctx = c.getContext("2d");
+		
+
+		//ogen
+		ctx.beginPath();
+		Module.eyecolor(<?php echo $_GET["facialExpression"];?>);
+		ctx.fillStyle = "Black";
+		ctx.fill();
+		//mond 
+		ctx.beginPath();
+		Module.emotion(<?php echo $_GET["facialExpression"];?>);	//THAFUQ
+		ctx.fillStyle = "Black";
+		ctx.fill();
+		ctx.closePath;
+		</script>
+			
+		<canvas id="BMI" width="155%"; height="auto"; style="background-color:#d9d9d9;"> </canvas>
+		<script>
+
+			Module.bmiToPicture(<?php echo $_GET["BMI"];?>);
+		</script>
+			
+
+		</div>
+		 
+
+        
+</div>
         
         
         
--- library.js
+++ library.js
@@ -1,34 +1,63 @@
-
-
 var Module = (function () {
-  
+    "use strict";
     return {
     hexToRgb: function (hex) {
     var bigint = parseInt(hex, 16);
     var r = (bigint >> 16) & 255;
     var g = (bigint >> 8) & 255;
     var b = bigint & 255;
-    return("rgb(" + r + ", " + g + ", " + b + ")");
+    return ("rgb(" + r + ", " + g + ", " + b + ")");
   },
       
     bmiToSize: function (bmi) {
       var Size = bmi * 2.5;
-      return(Size);
+      return (Size);
   },
         
   emotion: function (FE) {
-      var happy = "ctx.beginPath(); ctx.arc(90,75,5,0,2*Math.PI); ctx.fillStyle = 'Black'; ctx.fill(); ctx.beginPath(); ctx.arc(110,75,5,0,2*Math.PI); ctx.fillStyle = 'Black'; ctx.fill(); ctx.beginPath(); ctx.arc(100, 90, 20, 0, Math.PI, false); ctx.fillStyle = 'Black'; ctx.fill();";
-      var shocked = " ctx.beginPath(); ctx.arc(90,75,5,0,2*Math.PI); ctx.fillStyle = 'Black'; ctx.fill(); ctx.beginPath(); ctx.arc(110,75,5,0,2*Math.PI); ctx.fillStyle = 'Black'; ctx.fill(); ctx.beginPath(); ctx.arc(100, 105, 20, 0, Math.PI, true); ctx.fillStyle = 'Black'; ctx.fill();";
-      
-      if(FE = "happy"){
-          return(happy);
-      } else if (FE = "shocked")
-          return(shocked);
-      }
-  
-      
+	  if (FE == 1) {
+		var answer =  ctx.arc(85, 90, 20, 0, Math.PI, false);
+	  }
+	  if (FE == 2) {
+		var answer = ctx.arc(85, 105, 20, 0, Math.PI, true);
+	  }
+	  return (answer);
+  },
+		
+  eyecolor: function (FE) {
+	  if (FE == 1 || FE == 2) {
+		var answer =  ctx.arc(95,75,5,0,2*Math.PI);
+		var answer =  ctx.arc(75,75,5,0,2*Math.PI);
+	  }
 
-  };
+	  return (answer);
+  },
+		
+  bmiToPicture: function (bmi){
+	  
+	  var canvas = document.getElementById('BMI');
+      var context = canvas.getContext('2d');
+      var imageObj = new Image();
+	  var link= "";
+
+      imageObj.onload = function() {
+        context.drawImage(imageObj, 0, 0, 154, 154 * imageObj.height / imageObj.width);
+      };
+	  
+	  if (bmi <= 20){
+		  var link = 'http://i335404.iris.fhict.nl/images/man.jpg'
+	  }
+	  if (bmi > 20 && bmi < 28){
+		  var link = 'http://i335404.iris.fhict.nl/images/man-2.jpg'
+	  } 
+	  if (bmi >= 28){
+		  var link = 'http://i335404.iris.fhict.nl/images/man-3.jpg'
+	  } 
+      imageObj.src = link;
+  }
+
+
+};
 })();
 
 
